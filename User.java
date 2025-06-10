40b638156eff66625c69db94d71ee2781e9d6d25
zhaoyibu <3526797340@qq.com>
2025-06-10 21:28:12 +0800

    Create User.java

---
+++ User.java
@@ -0,0 +1,70 @@
+package com.example.model;
+
+import java.sql.Timestamp;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private int role;
+    private Timestamp createTime;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+} 
